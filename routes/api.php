41419165d4a915067dd5939259c40f7d88d4e8cb
raniaSayed <raniasayed313@gmail.com>
2025-05-17 12:16:34 +0300

    add sanctum package/ api.json

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\OrderController;
+use \App\Http\Controllers\ProductController;
 
 Route::middleware('auth:sanctum')->group(function () {
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/orders', [OrderController::class, 'store']);
-Route::get('/orders/{id}', [OrderController::class, 'show']);
+    Route::get('/api/products', [ProductController::class, 'index']);
+    Route::post('/api/orders', [OrderController::class, 'store']);
+    Route::get('/api/orders/{id}', [OrderController::class, 'show']);
 });
